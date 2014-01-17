f2163073eecf488b051d79604bb338d85f00cca8
cbv <cbv@Carls-MacBook-Pro.local>
2014-01-16 18:31:18 -0700

    Initial Commit

---
+++ OCMockExample/DEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DEAppDelegate.h
+//  OCMockExample
+//
+//  Created by cbv on 1/16/14.
+//  Copyright (c) 2014 Double Encore, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OCMockExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OCMockExample
+//
+//  Created by cbv on 1/16/14.
+//  Copyright (c) 2014 Double Encore, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DEAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DEAppDelegate class]));
+    }
+}
